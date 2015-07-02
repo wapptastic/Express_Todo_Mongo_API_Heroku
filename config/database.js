4c3bab84bc4c329c48b7258645e8075806c22960
Wapptastic <info@wapptastic.com>
2015-07-02 22:15:18 +0200

    first commit

--- config/database.js
+++ config/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-	url : 'mongodb://localhost/todo' || 'mongodb://heroku_t6cvr10h:sej3hnmobibaulp6oua2b1scfu@ds049548.mongolab.com:49548/heroku_t6cvr10h'
+	url : process.env.MONGOLAB_URI
 }
